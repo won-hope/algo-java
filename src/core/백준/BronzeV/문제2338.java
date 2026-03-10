0e5ab32718a24a3c697fcaf0d09a8c326d09f8c3
won-hope <hopedad.0604@gmail.com>
2026-03-10 12:32:20 +0900

    문제 풀이 : 브론즈4 ㄴ 2238 문제해결

---
+++ src/core/백준/BronzeV/문제2338.java
@@ -0,0 +1,23 @@
+package 백준.BronzeV;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class 문제2338 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+
+        // 덧셈
+        System.out.println(A.add(B));
+        // 뺼셈
+        System.out.println(A.subtract(B));
+        //곱셈
+        System.out.println(A.multiply(B));
+
+    }
+
+}
