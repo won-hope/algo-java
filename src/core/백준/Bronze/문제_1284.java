5677edc24e106f49b09291ba1c0b89689faad359
won-hope <hopedad.0604@gmail.com>
2026-04-01 15:48:32 +0900

    문제풀이

--- src/core/백준/Bronze/문제_1284.java
+++ src/core/백준/Bronze/문제_1284.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 public class 문제_1284 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         // TODO: solve here
 
